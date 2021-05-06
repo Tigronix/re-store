d156362cfc2518ac578296facf47391d3bd3cb85
Tigronix <vditold@yandex.ru>
2021-05-06 12:12:11 +0300

    отправка действий в redux store (action dispatch)

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -3,18 +3,7 @@ import BookList from '../book-list';
 
 const HomePage = () => {
   return (
-    <BookList books={[
-      {
-        id: 1,
-        title: 'Product Ready Microservices',
-        author: 'Susan J. Fowler'
-      },
-      {
-        id: 2,
-        title: 'Release It!',
-        author: 'Michael T. Nygard'
-      }
-    ]}></BookList>
+    <BookList></BookList>
   );
 };
 
