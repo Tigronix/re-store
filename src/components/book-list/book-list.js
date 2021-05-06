b728d2c37940732fcb0e23b19f5c46b2d70ded32
Tigronix <vditold@yandex.ru>
2021-05-06 14:24:43 +0300

    Redux: добавление элементов в массив

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,20 +3,24 @@ import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { fetchBooks } from '../../actions';
+import { fetchBooks, bookAddedToCart } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner/';
 import ErrorIndicator from '../error-indicator/';
 
 import './book-list.css';
 
-const BookList = ({ books }) => {
+const BookList = ({ books, onAddedToCart }) => {
   return (
     <ul className="book-list">
       {
         books.map((book) => {
           return (
-            <li key={book.id}><BookListItem book={book} /></li>
+            <li key={book.id}>
+              <BookListItem
+                book={book}
+                onAddedToCart={() => onAddedToCart(book.id)} />
+            </li>
           )
         })
       }
@@ -31,7 +35,7 @@ class BookListContainer extends Component {
   }
 
   render() {
-    const { books, loading, error } = this.props;
+    const { books, loading, error, onAddedToCart } = this.props;
 
     if (loading) {
       return <Spinner></Spinner>;
@@ -41,7 +45,7 @@ class BookListContainer extends Component {
       return <ErrorIndicator></ErrorIndicator>
     }
 
-    return <BookList books={books}></BookList>
+    return <BookList books={books} onAddedToCart={onAddedToCart}></BookList>
   }
 }
 
@@ -51,7 +55,8 @@ const mapStateToProps = ({ books, loading, error }) => {
 
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
   return {
-    fetchBooks: fetchBooks(bookstoreService, dispatch)
+    fetchBooks: fetchBooks(bookstoreService, dispatch),
+    onAddedToCart: (id) => dispatch(bookAddedToCart(id))
   };
 };
 
